5f4f645ddfb3e7ce57dcf7d860327e4bb0278a55
Lettever <jrafaelmtavares@gmail.com>
2025-06-26 18:56:56 +0100

    hi

--- src/lexer.dart
+++ src/lexer.dart
@@ -11,7 +11,7 @@ class Lexer {
     //int index = 0, line = 1, column = 0;
     List<String> errors = [];
 
-    final Map<String, TokenType> TokenTypeMap = {
+    final Map<String, TokenType> tokenTypeMap = {
         "=": TokenType.Equals,
         ";": TokenType.SemiColon,
         ".": TokenType.Dot,
@@ -19,24 +19,12 @@ class Lexer {
         "}": TokenType.R_Bracket,
     };
 
-    void advance(String span) {
-        pos.index += span.length;
-        pos.column += span.length;
-    }
-
-    Token makeAndAdvance(TokenType type, String span) {
-        pos.index += span.length;
-        pos.column += span.length;
-        return Token(type, span);
-    }
-
     Token next() {
         if (!source.canIndex(pos)) return Token(TokenType.EOF, "");
         if (shouldLexWhiteSpace()) return lexWhiteSpace();
         if (shouldLexMultiLineComment()) return lexMultiLineComment();
         if (shouldLexSingleLineComment()) return lexSingleLineComment();
         if (shouldLexDelimites()) return lexDelimiters();
-
         return Token(TokenType.Error, "this shouldn't happen");
     }
 
@@ -45,55 +33,50 @@ class Lexer {
     Token lexWhiteSpace() {
         while (source.canIndex(pos) && shouldLexWhiteSpace()) {
             String ch = source[pos.index];
-            pos.index += 1;
-            pos.column += 1;
-            if (ch == '\n') {
-                pos.line += 1;
-                pos.column = 0;
-            }
+            pos.advance(1);
+            if (ch == "\n") pos.nextLine();
         }
-        return Token(TokenType.White, "");
+        
+        return Token(TokenType.White, "" /*source.substring(i, pos.index)*/);
     }
 
-    bool shouldLexDelimites() => TokenTypeMap.containsKey(source[pos.index]);
+    bool shouldLexDelimites() => tokenTypeMap.containsKey(source[pos.index]);
 
     Token lexDelimiters() {
         String ch = source[pos.index];
-        return makeAndAdvance(TokenTypeMap[ch]!, ch);
+        pos.advance(ch.length);
+        return Token(tokenTypeMap[ch]!, ch);
     }
 
     bool shouldLexSingleLineComment() => source[pos.index] == '#';
 
     Token lexSingleLineComment() {
-        while (source.canIndex(pos) && (source[pos.index] != '\n')) {
-            pos.index += 1;
-            pos.column += 1;
-        }
-        pos.index += 1;
-        pos.column += 1;
+        while (source.canIndex(pos) && !source[pos.index].isNewLine()) pos.advance(1);
+        pos.advance(1);
+        pos.nextLine();
         return Token(TokenType.Comment, "");
     }
     
     bool shouldLexMultiLineComment() {
-        if (!source.canIndex(pos)) return false;
+        if (!source.canIndex(pos, 1)) return false;
         return source.substring(pos.index, pos.index + 2) == "#[";
     }
     
     Token lexMultiLineComment() {
-        print("\x1b[1;34m\x1b[1;41mNOT DONE\x1b[1;39m\x1b[1;49m");
-        pos.index += 2;
+        //print("\x1b[1;34m\x1b[1;41mNOT DONE\x1b[1;39m\x1b[1;49m");
+        pos.advance(2);
         int level = 1;
         while (source.canIndex(pos, 1) && level != 0) {
             String slice = source.substring(pos.index, pos.index + 2);
-            print(slice.length);
+            String ch = source[pos.index];
             if (slice == "#[") level += 1;
             else if (slice == "]#") level -= 1;
-            pos.column += 1;
-            if (source[pos.index] == "\n") pos.line += 1;
-            pos.index += 1;
+            pos.advance(1);
+            if (ch.isNewLine()) pos.nextLine();
         }
-        pos.index += 2;
-        return Token(TokenType.Comment, "");
+        pos.advance(2);
+        if (level != 0) return Token(TokenType.Error, "unclosed comment here");
+        return Token(TokenType.Comment, "1");
     }
 }
 
@@ -115,4 +98,19 @@ extension StringUtils on String {
         if (index < 0 || index >= runes.length) return null;
         return String.fromCharCode(runes[index]);
     }
+    bool isNewLine() => this == "\n";
 }
+
+/*
+    // Lambda assigned to a variable
+    var add = (int a, int b) => a + b;
+    print(add(2, 3)); // 5
+
+    // Equivalent to:
+    var addVerbose = (int a, int b) {
+    return a + b;
+    };
+
+    int Function(int, int) multiply = (int a, int b) => a * b;
+    print(multiply(4, 5)); // 20
+ */
--- src/lexer.pos.dart
+++ src/lexer.pos.dart
@@ -2,4 +2,22 @@ class LexerPosition {
     int index = 0;
     int line = 1;
     int column = 0;
+
+    void advance(int len) {
+        index += len;
+        column += len;
+    }
+
+    void nextLine() {
+        column = 0;
+        line += 1;
+    }
+
+    LexerPosition clone() {
+        var l = LexerPosition();
+        l.index = this.index;
+        l.column = this.column;
+        l.line = this.line;
+        return l;
+    }
 }
